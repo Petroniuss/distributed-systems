de3f21b44a2af7a3d5f661aebebabe221a5d4145
Petroniuss <patryk.wojtyczek@ibm.com>
2021-06-05 14:00:40 +0200

    designed data format

---
+++ lab7-gRPC/telemetry/java-client/build/generated/source/proto/main/grpc/iet/distributed/telemetry/ResourceMonitorGrpc.java
@@ -0,0 +1,278 @@
+package iet.distributed.telemetry;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.29.0)",
+    comments = "Source: telemetry.proto")
+public final class ResourceMonitorGrpc {
+
+  private ResourceMonitorGrpc() {}
+
+  public static final String SERVICE_NAME = "iet.distributed.telemetry.ResourceMonitor";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<iet.distributed.telemetry.BatchedData,
+      iet.distributed.telemetry.Acknowledment> getRouteChatMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "RouteChat",
+      requestType = iet.distributed.telemetry.BatchedData.class,
+      responseType = iet.distributed.telemetry.Acknowledment.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+  public static io.grpc.MethodDescriptor<iet.distributed.telemetry.BatchedData,
+      iet.distributed.telemetry.Acknowledment> getRouteChatMethod() {
+    io.grpc.MethodDescriptor<iet.distributed.telemetry.BatchedData, iet.distributed.telemetry.Acknowledment> getRouteChatMethod;
+    if ((getRouteChatMethod = ResourceMonitorGrpc.getRouteChatMethod) == null) {
+      synchronized (ResourceMonitorGrpc.class) {
+        if ((getRouteChatMethod = ResourceMonitorGrpc.getRouteChatMethod) == null) {
+          ResourceMonitorGrpc.getRouteChatMethod = getRouteChatMethod =
+              io.grpc.MethodDescriptor.<iet.distributed.telemetry.BatchedData, iet.distributed.telemetry.Acknowledment>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.BIDI_STREAMING)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "RouteChat"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  iet.distributed.telemetry.BatchedData.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  iet.distributed.telemetry.Acknowledment.getDefaultInstance()))
+              .setSchemaDescriptor(new ResourceMonitorMethodDescriptorSupplier("RouteChat"))
+              .build();
+        }
+      }
+    }
+    return getRouteChatMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static ResourceMonitorStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorStub>() {
+        @java.lang.Override
+        public ResourceMonitorStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ResourceMonitorStub(channel, callOptions);
+        }
+      };
+    return ResourceMonitorStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static ResourceMonitorBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorBlockingStub>() {
+        @java.lang.Override
+        public ResourceMonitorBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ResourceMonitorBlockingStub(channel, callOptions);
+        }
+      };
+    return ResourceMonitorBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static ResourceMonitorFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<ResourceMonitorFutureStub>() {
+        @java.lang.Override
+        public ResourceMonitorFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new ResourceMonitorFutureStub(channel, callOptions);
+        }
+      };
+    return ResourceMonitorFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public static abstract class ResourceMonitorImplBase implements io.grpc.BindableService {
+
+    /**
+     * <pre>
+     * client communicates with server using bidirectional async communication.
+     * </pre>
+     */
+    public io.grpc.stub.StreamObserver<iet.distributed.telemetry.BatchedData> routeChat(
+        io.grpc.stub.StreamObserver<iet.distributed.telemetry.Acknowledment> responseObserver) {
+      return asyncUnimplementedStreamingCall(getRouteChatMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getRouteChatMethod(),
+            asyncBidiStreamingCall(
+              new MethodHandlers<
+                iet.distributed.telemetry.BatchedData,
+                iet.distributed.telemetry.Acknowledment>(
+                  this, METHODID_ROUTE_CHAT)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class ResourceMonitorStub extends io.grpc.stub.AbstractAsyncStub<ResourceMonitorStub> {
+    private ResourceMonitorStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ResourceMonitorStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ResourceMonitorStub(channel, callOptions);
+    }
+
+    /**
+     * <pre>
+     * client communicates with server using bidirectional async communication.
+     * </pre>
+     */
+    public io.grpc.stub.StreamObserver<iet.distributed.telemetry.BatchedData> routeChat(
+        io.grpc.stub.StreamObserver<iet.distributed.telemetry.Acknowledment> responseObserver) {
+      return asyncBidiStreamingCall(
+          getChannel().newCall(getRouteChatMethod(), getCallOptions()), responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class ResourceMonitorBlockingStub extends io.grpc.stub.AbstractBlockingStub<ResourceMonitorBlockingStub> {
+    private ResourceMonitorBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ResourceMonitorBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ResourceMonitorBlockingStub(channel, callOptions);
+    }
+  }
+
+  /**
+   */
+  public static final class ResourceMonitorFutureStub extends io.grpc.stub.AbstractFutureStub<ResourceMonitorFutureStub> {
+    private ResourceMonitorFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected ResourceMonitorFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new ResourceMonitorFutureStub(channel, callOptions);
+    }
+  }
+
+  private static final int METHODID_ROUTE_CHAT = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final ResourceMonitorImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(ResourceMonitorImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_ROUTE_CHAT:
+          return (io.grpc.stub.StreamObserver<Req>) serviceImpl.routeChat(
+              (io.grpc.stub.StreamObserver<iet.distributed.telemetry.Acknowledment>) responseObserver);
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class ResourceMonitorBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    ResourceMonitorBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return iet.distributed.telemetry.Telemetry.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("ResourceMonitor");
+    }
+  }
+
+  private static final class ResourceMonitorFileDescriptorSupplier
+      extends ResourceMonitorBaseDescriptorSupplier {
+    ResourceMonitorFileDescriptorSupplier() {}
+  }
+
+  private static final class ResourceMonitorMethodDescriptorSupplier
+      extends ResourceMonitorBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    ResourceMonitorMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (ResourceMonitorGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new ResourceMonitorFileDescriptorSupplier())
+              .addMethod(getRouteChatMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
--- lab7-gRPC/telemetry/java-client/build/generated/source/proto/main/java/iet/distributed/telemetry/Telemetry.java
+++ lab7-gRPC/telemetry/java-client/build/generated/source/proto/main/java/iet/distributed/telemetry/Telemetry.java
@@ -15,6 +15,51 @@ public final class Telemetry {
         (com.google.protobuf.ExtensionRegistryLite) registry);
   }
   static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_Acknowledment_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_Acknowledment_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_BatchedData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_BatchedData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_SensorData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_SensorData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_SensorMetadata_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_SensorMetadata_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_Measurement_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_Measurement_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_CommonData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_CommonData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_TemperatureData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_TemperatureData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_WaterConsumptionData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_WaterConsumptionData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_iet_distributed_telemetry_PowerConsumptionData_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_iet_distributed_telemetry_PowerConsumptionData_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
     internal_static_iet_distributed_telemetry_Point_descriptor;
   static final 
     com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
@@ -54,73 +99,158 @@ public final class Telemetry {
   static {
     java.lang.String[] descriptorData = {
       "\n\017telemetry.proto\022\031iet.distributed.telem" +
-      "etry\032\036google/protobuf/duration.proto\",\n\005" +
-      "Point\022\020\n\010latitude\030\001 \001(\005\022\021\n\tlongitude\030\002 \001" +
-      "(\005\"g\n\tRectangle\022,\n\002lo\030\001 \001(\0132 .iet.distri" +
-      "buted.telemetry.Point\022,\n\002hi\030\002 \001(\0132 .iet." +
-      "distributed.telemetry.Point\"K\n\007Feature\022\014" +
-      "\n\004name\030\001 \001(\t\0222\n\010location\030\002 \001(\0132 .iet.dis" +
-      "tributed.telemetry.Point\"F\n\017FeatureDatab" +
-      "ase\0223\n\007feature\030\001 \003(\0132\".iet.distributed.t" +
-      "elemetry.Feature\"P\n\tRouteNote\0222\n\010locatio" +
-      "n\030\001 \001(\0132 .iet.distributed.telemetry.Poin" +
-      "t\022\017\n\007message\030\002 \001(\t\"}\n\014RouteSummary\022\023\n\013po" +
-      "int_count\030\001 \001(\005\022\025\n\rfeature_count\030\002 \001(\005\022\020" +
-      "\n\010distance\030\003 \001(\005\022/\n\014elapsed_time\030\004 \001(\0132\031" +
-      ".google.protobuf.Duration2\365\002\n\nRouteGuide" +
-      "\022R\n\nGetFeature\022 .iet.distributed.telemet" +
-      "ry.Point\032\".iet.distributed.telemetry.Fea" +
-      "ture\022Z\n\014ListFeatures\022$.iet.distributed.t" +
-      "elemetry.Rectangle\032\".iet.distributed.tel" +
-      "emetry.Feature0\001\022Z\n\013RecordRoute\022 .iet.di" +
-      "stributed.telemetry.Point\032\'.iet.distribu" +
-      "ted.telemetry.RouteSummary(\001\022[\n\tRouteCha" +
-      "t\022$.iet.distributed.telemetry.RouteNote\032" +
-      "$.iet.distributed.telemetry.RouteNote(\0010" +
-      "\001B\014P\001Z\010gen/;genb\006proto3"
+      "etry\032\036google/protobuf/duration.proto\032\037go" +
+      "ogle/protobuf/timestamp.proto\" \n\rAcknowl" +
+      "edment\022\017\n\007message\030\001 \001(\t\"\251\001\n\013BatchedData\022" +
+      "/\n\013batch_start\030\001 \001(\0132\032.google.protobuf.T" +
+      "imestamp\022-\n\tbatch_end\030\002 \001(\0132\032.google.pro" +
+      "tobuf.Timestamp\022:\n\013sensor_data\030\003 \003(\0132%.i" +
+      "et.distributed.telemetry.SensorData\"\277\001\n\n" +
+      "SensorData\022;\n\010metadata\030\001 \001(\0132).iet.distr" +
+      "ibuted.telemetry.SensorMetadata\0226\n\tdata_" +
+      "type\030\002 \001(\0162#.iet.distributed.telemetry.D" +
+      "ataType\022<\n\014measurements\030\003 \003(\0132&.iet.dist" +
+      "ributed.telemetry.Measurement\"-\n\016SensorM" +
+      "etadata\022\r\n\005owner\030\001 \001(\t\022\014\n\004type\030\002 \001(\t\"\306\002\n" +
+      "\013Measurement\022:\n\013common_data\030\001 \001(\0132%.iet." +
+      "distributed.telemetry.CommonData\022F\n\020temp" +
+      "erature_data\030\002 \001(\0132*.iet.distributed.tel" +
+      "emetry.TemperatureDataH\000\022Q\n\026water_consum" +
+      "ption_data\030\003 \001(\0132/.iet.distributed.telem" +
+      "etry.WaterConsumptionDataH\000\022Q\n\026power_con" +
+      "sumption_data\030\004 \001(\0132/.iet.distributed.te" +
+      "lemetry.PowerConsumptionDataH\000B\r\n\013one_of" +
+      "_data\";\n\nCommonData\022-\n\ttimestamp\030\001 \001(\0132\032" +
+      ".google.protobuf.Timestamp\"&\n\017Temperatur" +
+      "eData\022\023\n\013temperature\030\001 \001(\001\"%\n\024WaterConsu" +
+      "mptionData\022\r\n\005water\030\001 \001(\001\"%\n\024PowerConsum" +
+      "ptionData\022\r\n\005power\030\001 \001(\001\",\n\005Point\022\020\n\010lat" +
+      "itude\030\001 \001(\005\022\021\n\tlongitude\030\002 \001(\005\"g\n\tRectan" +
+      "gle\022,\n\002lo\030\001 \001(\0132 .iet.distributed.teleme" +
+      "try.Point\022,\n\002hi\030\002 \001(\0132 .iet.distributed." +
+      "telemetry.Point\"K\n\007Feature\022\014\n\004name\030\001 \001(\t" +
+      "\0222\n\010location\030\002 \001(\0132 .iet.distributed.tel" +
+      "emetry.Point\"F\n\017FeatureDatabase\0223\n\007featu" +
+      "re\030\001 \003(\0132\".iet.distributed.telemetry.Fea" +
+      "ture\"P\n\tRouteNote\0222\n\010location\030\001 \001(\0132 .ie" +
+      "t.distributed.telemetry.Point\022\017\n\007message" +
+      "\030\002 \001(\t\"}\n\014RouteSummary\022\023\n\013point_count\030\001 " +
+      "\001(\005\022\025\n\rfeature_count\030\002 \001(\005\022\020\n\010distance\030\003" +
+      " \001(\005\022/\n\014elapsed_time\030\004 \001(\0132\031.google.prot" +
+      "obuf.Duration*1\n\010DataType\022\017\n\013TEMPERATURE" +
+      "\020\000\022\t\n\005WATER\020\001\022\t\n\005POWER\020\0022t\n\017ResourceMoni" +
+      "tor\022a\n\tRouteChat\022&.iet.distributed.telem" +
+      "etry.BatchedData\032(.iet.distributed.telem" +
+      "etry.Acknowledment(\0010\0012\365\002\n\nRouteGuide\022R\n" +
+      "\nGetFeature\022 .iet.distributed.telemetry." +
+      "Point\032\".iet.distributed.telemetry.Featur" +
+      "e\022Z\n\014ListFeatures\022$.iet.distributed.tele" +
+      "metry.Rectangle\032\".iet.distributed.teleme" +
+      "try.Feature0\001\022Z\n\013RecordRoute\022 .iet.distr" +
+      "ibuted.telemetry.Point\032\'.iet.distributed" +
+      ".telemetry.RouteSummary(\001\022[\n\tRouteChat\022$" +
+      ".iet.distributed.telemetry.RouteNote\032$.i" +
+      "et.distributed.telemetry.RouteNote(\0010\001B\014" +
+      "P\001Z\010gen/;genb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
           com.google.protobuf.DurationProto.getDescriptor(),
+          com.google.protobuf.TimestampProto.getDescriptor(),
         });
-    internal_static_iet_distributed_telemetry_Point_descriptor =
+    internal_static_iet_distributed_telemetry_Acknowledment_descriptor =
       getDescriptor().getMessageTypes().get(0);
+    internal_static_iet_distributed_telemetry_Acknowledment_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_Acknowledment_descriptor,
+        new java.lang.String[] { "Message", });
+    internal_static_iet_distributed_telemetry_BatchedData_descriptor =
+      getDescriptor().getMessageTypes().get(1);
+    internal_static_iet_distributed_telemetry_BatchedData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_BatchedData_descriptor,
+        new java.lang.String[] { "BatchStart", "BatchEnd", "SensorData", });
+    internal_static_iet_distributed_telemetry_SensorData_descriptor =
+      getDescriptor().getMessageTypes().get(2);
+    internal_static_iet_distributed_telemetry_SensorData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_SensorData_descriptor,
+        new java.lang.String[] { "Metadata", "DataType", "Measurements", });
+    internal_static_iet_distributed_telemetry_SensorMetadata_descriptor =
+      getDescriptor().getMessageTypes().get(3);
+    internal_static_iet_distributed_telemetry_SensorMetadata_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_SensorMetadata_descriptor,
+        new java.lang.String[] { "Owner", "Type", });
+    internal_static_iet_distributed_telemetry_Measurement_descriptor =
+      getDescriptor().getMessageTypes().get(4);
+    internal_static_iet_distributed_telemetry_Measurement_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_Measurement_descriptor,
+        new java.lang.String[] { "CommonData", "TemperatureData", "WaterConsumptionData", "PowerConsumptionData", "OneOfData", });
+    internal_static_iet_distributed_telemetry_CommonData_descriptor =
+      getDescriptor().getMessageTypes().get(5);
+    internal_static_iet_distributed_telemetry_CommonData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_CommonData_descriptor,
+        new java.lang.String[] { "Timestamp", });
+    internal_static_iet_distributed_telemetry_TemperatureData_descriptor =
+      getDescriptor().getMessageTypes().get(6);
+    internal_static_iet_distributed_telemetry_TemperatureData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_TemperatureData_descriptor,
+        new java.lang.String[] { "Temperature", });
+    internal_static_iet_distributed_telemetry_WaterConsumptionData_descriptor =
+      getDescriptor().getMessageTypes().get(7);
+    internal_static_iet_distributed_telemetry_WaterConsumptionData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_WaterConsumptionData_descriptor,
+        new java.lang.String[] { "Water", });
+    internal_static_iet_distributed_telemetry_PowerConsumptionData_descriptor =
+      getDescriptor().getMessageTypes().get(8);
+    internal_static_iet_distributed_telemetry_PowerConsumptionData_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_iet_distributed_telemetry_PowerConsumptionData_descriptor,
+        new java.lang.String[] { "Power", });
+    internal_static_iet_distributed_telemetry_Point_descriptor =
+      getDescriptor().getMessageTypes().get(9);
     internal_static_iet_distributed_telemetry_Point_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_Point_descriptor,
         new java.lang.String[] { "Latitude", "Longitude", });
     internal_static_iet_distributed_telemetry_Rectangle_descriptor =
-      getDescriptor().getMessageTypes().get(1);
+      getDescriptor().getMessageTypes().get(10);
     internal_static_iet_distributed_telemetry_Rectangle_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_Rectangle_descriptor,
         new java.lang.String[] { "Lo", "Hi", });
     internal_static_iet_distributed_telemetry_Feature_descriptor =
-      getDescriptor().getMessageTypes().get(2);
+      getDescriptor().getMessageTypes().get(11);
     internal_static_iet_distributed_telemetry_Feature_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_Feature_descriptor,
         new java.lang.String[] { "Name", "Location", });
     internal_static_iet_distributed_telemetry_FeatureDatabase_descriptor =
-      getDescriptor().getMessageTypes().get(3);
+      getDescriptor().getMessageTypes().get(12);
     internal_static_iet_distributed_telemetry_FeatureDatabase_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_FeatureDatabase_descriptor,
         new java.lang.String[] { "Feature", });
     internal_static_iet_distributed_telemetry_RouteNote_descriptor =
-      getDescriptor().getMessageTypes().get(4);
+      getDescriptor().getMessageTypes().get(13);
     internal_static_iet_distributed_telemetry_RouteNote_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_RouteNote_descriptor,
         new java.lang.String[] { "Location", "Message", });
     internal_static_iet_distributed_telemetry_RouteSummary_descriptor =
-      getDescriptor().getMessageTypes().get(5);
+      getDescriptor().getMessageTypes().get(14);
     internal_static_iet_distributed_telemetry_RouteSummary_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_iet_distributed_telemetry_RouteSummary_descriptor,
         new java.lang.String[] { "PointCount", "FeatureCount", "Distance", "ElapsedTime", });
     com.google.protobuf.DurationProto.getDescriptor();
+    com.google.protobuf.TimestampProto.getDescriptor();
   }
 
   // @@protoc_insertion_point(outer_class_scope)
